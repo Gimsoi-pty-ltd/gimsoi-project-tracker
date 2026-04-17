d29625aaecec91696b6b86aa7f8ffa69c9db9c43
TshegofatsoMkhabela <tshego300@gmail.com>
2026-04-17 07:32:55 +0200

    chore: clean up comments

--- apps/backend/utils/jwt.utils.js
+++ apps/backend/utils/jwt.utils.js
@@ -7,9 +7,6 @@ export const generateTokenAndSetCookie = (res, userId, role) => {
         { expiresIn: "7d", algorithm: "HS256" }
     );
 
-    // AppSail doesn't always inject NODE_ENV properly in early boot,
-    // and since frontend (Slate) and backend (AppSail) are always separate domains,
-    // we MUST force SameSite=none and secure=true.
     res.cookie("token", token, {
         httpOnly: true, // Prevents XSS
         secure: true,
